c4a3198d6c7306e0befe24394e6fcc519f665fdc
Willian Souza <willianaru@gmail.com>
2020-05-05 03:20:38 -0300

    Ajuste nas rotas

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
+// Require express and create an express application instance
 const express = require("express");
+const app = express();
+
+// Require the express routes defined in router.js
+const routes = require("./router");
+
+// Define the hostname and port where the server can be found
+const port = 5000;
+
+// Define the directory where static files are found
+app.use(express.static("public"));
+
+// Specify the routes to be used for our application
+app.use(routes);
+
+// Begin accepting connections to the specified port
+app.listen(port, () => {
+  // Display server location information to the console
+  console.log(`Server is listening at  ${port}`);
+});
+
+/* const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
@@ -8,3 +30,4 @@ express()
   .set("view engine", "ejs")
   .get("/", (req, res) => res.render("pages/index"))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+ */
