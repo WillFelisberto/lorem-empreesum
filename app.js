8066b14bdaea694e745ccf674889a6b4a576c60c
Willian Souza <willianaru@gmail.com>
2020-05-05 03:18:00 -0300

    Ajuste start dessa merda 2

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-// Require express and create an express application instance
-const express = require('express');
-const app = express();
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
-// Require the express routes defined in router.js
-const routes = require('./router');
-
-// Define the hostname and port where the server can be found
-const hostname = "127.0.0.1";
-const port = 3000;
-
-// Define the directory where static files are found
-app.use(express.static('public'));
-
-// Specify the routes to be used for our application 
-app.use(routes);
-
-// Begin accepting connections to the specified port
-app.listen(port, () => {
-  // Display server location information to the console
-  console.log(`Server is listening at http://${hostname}:${port}/`);
-})
+express()
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
